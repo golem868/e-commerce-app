14ebed350daec70ab14a6407f50f8a576e68d0a1
golem868 <58805020+golem868@users.noreply.github.com>
2020-05-20 18:39:40 +1200

    Updates

---
+++ precache-manifest.3dbc3d9e00dcb2c5a59c50a37b86e6d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75694bd0c1156e4ded9bcbc5cd900036",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb871ca875219e4098b0",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f10b719ba89f93c2d3d7",
+    "url": "/static/css/main.84a539b6.chunk.css"
+  },
+  {
+    "revision": "fb871ca875219e4098b0",
+    "url": "/static/js/2.9d12cef4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d12cef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f10b719ba89f93c2d3d7",
+    "url": "/static/js/main.160d0b4c.chunk.js"
+  },
+  {
+    "revision": "5a49031ce15b0559ce0b",
+    "url": "/static/js/runtime-main.a46ac097.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
